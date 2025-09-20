689e869f7e496b7c6d58197e2ecf0f886ee103ea
vodobryshkin <dobryshkinvladimir@yandex.ru>
2025-09-20 13:22:48 +0300

    created DTO from Point

--- src/main/java/geometry/model/Point.java
+++ src/main/java/geometry/model/Point.java
@@ -1,7 +1,6 @@
 package geometry.model;
 
 import lombok.Data;
-import lombok.Getter;
 
 import java.math.BigDecimal;
 
@@ -9,7 +8,7 @@ import java.math.BigDecimal;
  * Класс-абстракция над точкой на координатной плоскости.
  * Нужен для обработки данных на бэкенде.
  */
-@Getter @Data
+@Data
 public class Point {
     private final BigDecimal x;
     private final BigDecimal y;
