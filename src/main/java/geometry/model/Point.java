692fb687af11108acea6d827551ac3eb8324f3fb
vodobryshkin <dobryshkinvladimir@yandex.ru>
2025-09-20 13:11:50 +0300

    crated DTO from Point

--- src/main/java/geometry/model/Point.java
+++ src/main/java/geometry/model/Point.java
@@ -1,40 +1,16 @@
 package geometry.model;
 
+import lombok.Data;
 import lombok.Getter;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
 /**
  * Класс-абстракция над точкой на координатной плоскости.
  * Нужен для обработки данных на бэкенде.
  */
-@Getter
+@Getter @Data
 public class Point {
     private final BigDecimal x;
     private final BigDecimal y;
-
-    /**
-     * Конструктор класса Point
-     *
-     * @param x значение по оси x.
-     * @param y значение по оси y.
-     */
-    public Point(BigDecimal x, BigDecimal y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Point(%f, %f)", x, y);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o instanceof Point point1) {
-            return this.x.equals(point1.x) && Objects.equals(y, point1.y);
-        }
-        return false;
-    }
 }
